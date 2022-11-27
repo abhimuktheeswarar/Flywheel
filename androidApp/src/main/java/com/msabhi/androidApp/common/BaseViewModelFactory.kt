2fdcf4e5d065c5a9a389a7763cefb41d633ee757
Abhi Muktheeswarar <15965088+abhimuktheeswarar@users.noreply.github.com>
2022-11-27 14:29:19 +0530

    Update Android dependency versions

--- androidApp/src/main/java/com/msabhi/androidApp/common/BaseViewModelFactory.kt
+++ androidApp/src/main/java/com/msabhi/androidApp/common/BaseViewModelFactory.kt
@@ -22,7 +22,7 @@ import androidx.lifecycle.ViewModelProvider
 class BaseViewModelFactory<T>(val creator: () -> T) : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return creator() as T
     }
 }
--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -28,19 +28,19 @@ object Versions {
         const val buildTools = "30.0.3"
         const val androidGradlePlugin = "7.3.1"
 
-        const val appCompat = "1.3.0"
-        const val core = "1.6.0"
-        const val activityKtx = "1.2.3"
-        const val fragmentKtx = "1.3.5"
-        const val constraintLayout = "2.0.4"
-        const val lifecycle = "2.3.1"
-        const val recyclerView = "1.2.0"
-        const val materialComponents = "1.4.0"
+        const val appCompat = "1.5.1"
+        const val core = "1.9.0"
+        const val activityKtx = "1.6.1"
+        const val fragmentKtx = "1.5.4"
+        const val constraintLayout = "2.1.4"
+        const val lifecycle = "2.5.1"
+        const val recyclerView = "1.2.1"
+        const val materialComponents = "1.7.0"
 
-        const val navigation = "2.3.5"
+        const val navigation = "2.5.3"
 
-        const val test = "1.3.0"
-        const val testExt = "1.1.3"
+        const val test = "1.5.0"
+        const val testExt = "1.1.4"
     }
 
     object Testing {
@@ -92,9 +92,9 @@ object Dependencies {
             "com.google.android.material:material:${Versions.Android.materialComponents}"
 
         const val navigationFragment =
-            "androidx.navigation:navigation-fragment-ktx${Versions.Android.navigation}"
+            "androidx.navigation:navigation-fragment-ktx:${Versions.Android.navigation}"
         const val navigationUi =
-            "androidx.navigation:navigation-ui-ktx${Versions.Android.navigation}"
+            "androidx.navigation:navigation-ui-ktx:${Versions.Android.navigation}"
     }
 
     object AndroidTest {
