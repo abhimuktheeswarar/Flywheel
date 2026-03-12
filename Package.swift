1694a1f54e494e3921f08ed8cdaf542e2d303cd1
Abhi Muktheeswarar <abhi.muktheeswarar@redbus.com>
2026-03-12 18:13:10 +0530

    Release v1.1.6 for SPM

--- Package.swift
+++ Package.swift
@@ -1,16 +1,6 @@
 // swift-tools-version:5.9
 import PackageDescription
 
-// Local-development Package.swift.
-//
-// The XCFramework referenced below is NOT checked into git.
-// To populate it locally, run:
-//   ./gradlew :flywheel:copyXCFrameworkToRepo
-//
-// For release/remote SPM consumption (URL + checksum), run:
-//   ./gradlew :flywheel:generatePackageSwift
-// and use the generated file at flywheel/build/spm/Package.swift
-
 let package = Package(
     name: "Flywheel",
     platforms: [.iOS(.v16), .watchOS(.v9), .tvOS(.v16), .macOS(.v13)],
@@ -23,7 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Flywheel",
-            path: "flywheel/xcframework/Flywheel.xcframework"
+            url: "https://github.com/abhimuktheeswarar/Flywheel/releases/download/v1.1.6/Flywheel.xcframework.zip",
+            checksum: "866703fd1845edc4a83a2d346d8291d5fbc0f8090533700f726c64eb35e9606e"
         ),
     ]
 )
